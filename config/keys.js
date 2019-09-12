a9f60a5fabb496e034d3179355d6d8370d6f6319
yura1985 <yaremayurii@gmail.com>
2019-09-12 17:45:05 -0400

    Set up a authentication 1- install dependencies 2- create routes/api/auth.js file 3- create routes/api/users.js file 4- create models/User.js file 5- create middleware/auth.js file 6- create config/default.json file

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://yura1985:RtnUci@vNP5$.N3@cluster0-wf3wo.mongodb.net/test?retryWrites=true&w=majority'
-}
--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express =require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const config = require('config');
+
+const tasks = require('./routes/api/tasks');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
 
 const app = express();
 
 //BodyParser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB cofig
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 //conecting to mongodb
 mongoose
   .connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true
   }) // Adding new mongo url parser
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
+
+// use routes
+app.use('/api/tasks', tasks)
+app.use('/api/users', users)
+app.use('/api/auth', auth)
 
    const port = process.env.PORT || 5000
 
